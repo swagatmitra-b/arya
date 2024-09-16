b2581e18e7e56ad6d7f3b05dfca96949bacf6ef6
Swagat <swagatmitrabhattacharya18@gmail.com>
2024-09-16 16:05:53 +0530

    midterms demain, je suis vraiment maniaque

--- src/postfix.ts
+++ src/postfix.ts
@@ -52,7 +52,8 @@ export function parseP(s: string) {
   return output;
 }
 
-export function evaluateP(vals: Record<string, number>, s: string): number {
+export function evaluateExp(vals: Record<string, number>, s: string): number {
+  s = fixString(s);
   const pf = parseP(s);
   let stack = [];
   for (let i = 0; i < pf.length; i++) {
@@ -86,4 +87,63 @@ export function evaluateP(vals: Record<string, number>, s: string): number {
     } else stack.push(token);
   }
   return round((stack.pop() as number) * 100) / 100;
+}
+
+const isChar = (s: string, i: number) =>
+  !operators.includes(s[i]) && s[i] != "(" && s[i] != ")";
+
+const satisfyNum = (s: string, si: number, ti: number) =>
+  isNaN(parseFloat(s[si])) || isNaN(parseFloat(s[ti]));
+
+function fixString(s: string) {
+  s = [...s].filter((str) => str != " ").join("");
+  let positions = new Set<number>();
+
+  for (let i = 0; i < s.length; i++) {
+    if (isChar(s, i)) {
+      if (i == 0) {
+        if (
+          !operators.includes(s[i + 1]) &&
+          s[i + 1] != ")" &&
+          satisfyNum(s, i, i + 1)
+        ) {
+          positions.add(i + 1);
+        }
+      } else if (i == s.length - 1) {
+        if (
+          !operators.includes(s[i - 1]) &&
+          s[i - 1] != "(" &&
+          satisfyNum(s, i, i - 1)
+        ) {
+          positions.add(i);
+        }
+      } else {
+        if (
+          !operators.includes(s[i + 1]) &&
+          s[i + 1] != ")" &&
+          satisfyNum(s, i, i + 1)
+        ) {
+          positions.add(i + 1);
+        }
+        if (
+          !operators.includes(s[i - 1]) &&
+          s[i - 1] != "(" &&
+          satisfyNum(s, i, i - 1)
+        ) {
+          positions.add(i);
+        }
+      }
+    }
+  }
+  return applyString(s, positions);
+}
+
+function applyString(s: string, positions: Set<number>) {
+  let indices = Array.from(positions.values()).sort((a, b) => a - b);
+  let b = [...s];
+  let len = b.length;
+  for (let i = 0; i < indices.length; i++) {
+    b.splice(b.length - len + indices[i], 0, "*");
+  }
+  return b.join("");
 }
--- tests/postfixParse.test.ts
+++ tests/postfixParse.test.ts
@@ -1,5 +1,5 @@
 import { yes } from "./helper";
-import { evaluateP, parseP } from "../src/postfix";
+import { parseP, evaluateExp } from "../src/postfix";
 
 export function postfixTest() {
   yes("1", parseP("A+B*C-D"), "ABC*+D-");
@@ -12,7 +12,7 @@ export function postfixTest() {
 export function evalPostFixTest() {
   yes(
     "1",
-    evaluateP(
+    evaluateExp(
       {
         A: 1,
         B: 2,
@@ -25,7 +25,7 @@ export function evalPostFixTest() {
   );
   yes(
     "2",
-    evaluateP(
+    evaluateExp(
       {
         A: 2,
         B: 3,
@@ -37,8 +37,8 @@ export function evalPostFixTest() {
     7.33
   );
   yes(
-    "5",
-    evaluateP(
+    "3",
+    evaluateExp(
       {
         A: 2,
         B: 4,
@@ -50,5 +50,16 @@ export function evalPostFixTest() {
       "A/B+(C-D)*E^(F-2*3)+3*8"
     ),
     24.5
+  );
+  yes(
+    "4",
+    evaluateExp(
+      {
+        x: 2,
+        y: 3,
+      },
+      "2x + y(2x^y + yx) + 8y(y+2)"
+    ),
+    190
   );
 }
